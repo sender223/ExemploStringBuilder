2a6fa77dc5a77404e6588ac2a1d10d1d3bf330d9
Sender Rodrigues dos Santos <sender.rsantos@gmail.com>
2019-10-03 13:35:23 -0300

    Iniciado

---
+++ ExemploStringBuilder/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ExemploStringBuilder {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
